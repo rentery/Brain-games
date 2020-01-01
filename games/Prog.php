6c0e2cf2e9990765f796138522d3ba4c589da253
Gleb <gleb__l@mail.ru>
2020-01-02 00:08:27 +0300

    add brain-progression game

---
+++ games/Prog.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BrainGames\Prog;
+
+use function BrainGames\Cli\game;
+use function BrainGames\Cli\welcome;
+
+
+
+function prog()
+{
+    $gameRule = 'What number is missing in the progression?';
+    $userName = welcome($gameRule);
+    $progression = range(5, 23, 2);
+    for ($i = 0; $i < 3; $i++) {
+        $question = "";
+        $char = array_rand($progression);
+        $correctAnswers[] = $progression[$char];
+        foreach ($progression as $key => $value) {
+            if ($key == $char) {
+                $question .= ".. ";
+            } else {
+                $question .= "{$value} ";
+            }
+        }
+        $questions[] = trim($question);
+    }
+    $gameData = [$userName, $questions, $correctAnswers];
+    game($gameData);
+}
