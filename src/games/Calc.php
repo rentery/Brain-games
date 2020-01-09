8c7a409a5a93dec152ab1063f3ff0134f5bddd54
Gleb <gleb__l@mail.ru>
2020-01-09 23:51:21 +0300

    fix calc.php

--- src/games/Calc.php
+++ src/games/Calc.php
@@ -6,8 +6,7 @@ use function BrainGames\Cli\startGame;
 use const BrainGames\Cli\ROUND_COUNT;
 
 const BRAIN_CALC_RULE = 'What is the result of the expression?';
-
-$operators = ['+', '-', '*'];
+const OPERATORS = ['+', '-', '*'];
 
 function calculateExpression($operator, $operand1, $operand2)
 {
@@ -21,13 +20,13 @@ function calculateExpression($operator, $operand1, $operand2)
     }
 }
 
-function calc($operators)
+function calc()
 {
     for ($i = 0; $i < ROUND_COUNT; $i++) {
         $operand1 = rand(1, 50);
         $operand2 = rand(1, 50);
-        $randOperator = array_rand($operators);
-        $operator = $operators[$randOperator];
+        $randOperator = array_rand(OPERATORS);
+        $operator = OPERATORS[$randOperator];
         $correctAnswers[] = calculateExpression($operator, $operand1, $operand2);
         $questions[] = "{$operand1} {$operator} {$operand2}";
     }
