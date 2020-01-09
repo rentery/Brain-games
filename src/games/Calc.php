72f66daf95dc7f633580010a0463cbed42a0b3b1
Gleb <gleb__l@mail.ru>
2020-01-09 23:30:29 +0300

    rename calculate function

--- src/games/Calc.php
+++ src/games/Calc.php
@@ -7,7 +7,9 @@ use const BrainGames\Cli\ROUND_COUNT;
 
 const BRAIN_CALC_RULE = 'What is the result of the expression?';
 
-function resultOfCulc($operator, $operand1, $operand2)
+$operators = ['+', '-', '*'];
+
+function calculateExpression($operator, $operand1, $operand2)
 {
     switch ($operator) {
         case '+':
@@ -19,15 +21,14 @@ function resultOfCulc($operator, $operand1, $operand2)
     }
 }
 
-function calc()
+function calc($operators)
 {
-    $operators = ['+', '-', '*'];
     for ($i = 0; $i < ROUND_COUNT; $i++) {
         $operand1 = rand(1, 50);
         $operand2 = rand(1, 50);
         $randOperator = array_rand($operators);
         $operator = $operators[$randOperator];
-        $correctAnswers[] = resultOfCulc($operator, $operand1, $operand2);
+        $correctAnswers[] = calculateExpresson($operator, $operand1, $operand2);
         $questions[] = "{$operand1} {$operator} {$operand2}";
     }
     $gameData = [BRAIN_CALC_RULE, $questions, $correctAnswers];
